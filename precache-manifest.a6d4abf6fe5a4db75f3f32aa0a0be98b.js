d2613ebab59e3e28b92317c788114b3d5959e1be
dhica14 <49179368+dhica14@users.noreply.github.com>
2021-01-20 12:10:36 +0900

    Add files via upload

---
+++ precache-manifest.a6d4abf6fe5a4db75f3f32aa0a0be98b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b46430ce44eb31a3d004",
+    "url": "/static/js/main.4d84d6f8.chunk.js"
+  },
+  {
+    "revision": "1f7cac902d68ab6fba12",
+    "url": "/static/js/2.2453fbe7.chunk.js"
+  },
+  {
+    "revision": "b46430ce44eb31a3d004",
+    "url": "/static/css/main.0b283144.chunk.css"
+  },
+  {
+    "revision": "be455ef71a1256a7a38041b9832c38c7",
+    "url": "/index.html"
+  }
+];
